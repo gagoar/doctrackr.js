25b2662b84d77abbadc4d22c7768c949c544c8b7
gagoar <xeroice@gmail.com>
2013-08-29 01:08:10 -0400

    better events than callbacks.

--- js/DT.js
+++ js/DT.js
@@ -7,8 +7,6 @@ DT = (function (){
   }
   // origin url for diverse uses
   var origin = window.location.href.replace(window.location.pathname, '')
-  // secret given in the init function
-  , secret = undefined
   // code obtain in the 1 fase of oauth2 authentication
   , code = undefined
   // token structure
@@ -19,11 +17,19 @@ DT = (function (){
   , valid = false
   // config for authorize
   var config =  {
-    client_id: '4d4aa4e01126e54003e5f4f60034a1467940ba038af702d61716e709ae4a37c7'
-    , url: 'https://use.doctrackr.com/oauth/authorize'
+    url: 'https://use.doctrackr.com/oauth/authorize'
     , token_url: 'https://api.doctrackr.com/oauth/token'
-    , redirect_uri: 'http://localhost:9010/callback.html'
     , response_type: 'code'
+    app: {
+      client_id: undefined
+      , redirect_uri: undefined
+      , secret: undefined
+    }
+    check: function(){
+      for (var property in config.app)
+        if (config.app[property] == '' || config.app[property] == undefined) throw new Error('check config and try again');
+      return true
+    }
   }
   // Policies store
   var privileges = {
@@ -32,13 +38,18 @@ DT = (function (){
     , can_print: []
   }
 
-  // initialize the library
-  var init = function (client_secret){
-    if (client_secret){
-      logout()
-      secret = client_secret
-      if(console) console.warn('all set!')
-    }else throw new Error('No client secret given')
+  // initializer
+  var init = function (app_config){
+    if ( attr in app_config){
+
+      for(var attr in app_config) // iterar sobre esto para chequear que la conf esta bien asignada.
+        config.app[attr] = app_config[attr];
+
+      config.check() // checking conf first.
+      events.create = [ 'login', 'logout', 'token', 'status', 'createFile', 'createPolicy', 'updatePolicy'];
+      console.warn('all set!')
+
+    }else throw new Error('check configuration and try again')
 
   }
 
@@ -59,6 +70,38 @@ DT = (function (){
     return params.join('&')
   }
 
+  //the event bringer. Because having 'callbacks' it's the worst thing ever
+  var events = {
+    events: {},
+    set create(evts) {
+      var dom = window.document;
+      var family = 'dt';
+      if (window.CustomEvent) {
+        for (var i = 0, event; event = evts[i]; i++)
+          events[event] = {
+            success: new CustomEvent(['success', event, family].join('.')),
+            fail: new CustomEvent(['fail', event, family].join('.'))
+          }
+      } else {
+        // IE impl. for throwing events
+        for (var i = 0, event; event = evts[i]; i++)
+          events[event] = {
+            success: dom.createElement('Event')
+              .initEvent(['success', event, family].join('.'), false, false),
+            fail: dom.createElement('Event')
+              .initEvent(['fail', event, family].join('.'), false, false)
+          }
+      }
+    },
+    set trigger(evt) {
+      var dom = window.document;
+      var event = events[evt[0]][evt[1]];
+      event.data = evt[2]
+      dom.dispatchEvent?
+        dom.dispatchEvent(event)
+        : dom.fireEvent('on' + event.eventType, event);
+    }
+  };
   // get well formed url for lunching docktrackr authorization
   var auth = function (){
     return [ config.url, paramsProcessor(config) ].join('?')
@@ -87,7 +130,11 @@ DT = (function (){
   // storeToken => store the token along with all the related data that comes with it
   var storeToken = function (e){
     tokenData = JSON.parse(e.target.response);
-    if (tokenData.access_token) setTimeOutToken()
+    if (tokenData.access_token){
+      setTimeOutToken()
+      events.trigger = ['login','success', { access_token: tokenData.access_token, expires_in: tokenData.expires_in }]
+    }else
+      events.trigger = [ 'login', 'fail']
   }
 
   // destroy token when expires
@@ -102,16 +149,17 @@ DT = (function (){
     return privileges.can_edit.length || privileges.can_print.length || privileges.can_view.length
   }
 
-  // responsePolicy
-  var responsePolicy = function(callback){
-    return function(e){
-      console.log(e)
-      callback(e)
-    }
+  // standard Answer
+  var policyAnswer = function(type){
+   return function(e){
+     console.log(e);
+     result  = event.status == 200 ? 'success' : 'fail'
+     events.trigger = [type, result, e]
+   }
   }
 
   // createAPolicy
-  var createAPolicy = function (callback) {
+  var createAPolicy = function () {
     if ( ! anyPrivileges() ) throw new Error('no privilages given')
 
     var xhr = new XMLHttpRequest()
@@ -124,7 +172,7 @@ DT = (function (){
 
     xhr.open('POST', 'https://api.doctrackr.com/v1/policies', true);
     xhr.setRequestHeader('Authorization', ['Bearer', returnToken()].join(' '))
-    xhr.onload = responsePolicy(callback)
+    xhr.onload = standardAnswer('createPolicy')
     xhr.send(formData)
   }
 
@@ -134,7 +182,7 @@ DT = (function (){
 
   }
   // updatePolicy
-  var updatePolicy = function (policy_id, callback) {
+  var updatePolicy = function (policy_id) {
     if ( policy_id && ! anyPrivileges() ) throw new Error('check policy_id given and try again')
 
     var xhr = new XMLHttpRequest()
@@ -146,7 +194,7 @@ DT = (function (){
 
     xhr.open('PUT', 'https://api.doctrackr.com/v1/policies/' + policy_id, true);
     xhr.setRequestHeader('Authorization', ['Bearer', returnToken()].join(' '))
-    xhr.onload = responsePolicy(callback)
+    xhr.onload = policyAnswer('updatePolicy')
     xhr.send(formData)
   }
 
@@ -195,13 +243,13 @@ DT = (function (){
   }
 
   // getProtectedFile
-  var getProtectedFile = function (id, url, name, callback) {
+  var getProtectedFile = function (id, url, name) {
     console.log('DONE:', id, url, name)
-    callback(id, url, name)
+    events.trigger = ['createFile', 'success' , {id: id, url: url, name: name}]
   }
 
   // getStatus
-  var getStatus = function (id, callback) {
+  var getStatus = function (id) {
     return function(){
       var xhr = new XMLHttpRequest()
       , action = ['https://api.doctrackr.com/v1/documents/', id].join('')
@@ -209,32 +257,33 @@ DT = (function (){
 
       xhr.open('GET', action , true);
       xhr.setRequestHeader('Authorization', ['Bearer', returnToken()].join(' '))
-      xhr.onload = getResponseFile(callback)
+      xhr.onload = getResponseFile()
       xhr.send()
     }
   }
 
   // checkForStatus
-  var checkForStatus = function (id, delay, callback) {
+  var checkForStatus = function (id, delay) {
     console.log('checkForStatus:', id, delay)
-    setTimeout( getStatus(id, callback), delay)
+    events.trigger = ['status', 'success', {id: id, delay: delay}]
+    setTimeout( getStatus(id), delay)
   }
 
   // getResponseFile
-  var getResponseFile = function (callback) {
+  var getResponseFile = function () {
     return function (e){
       var resp = JSON.parse(e.target.response)
       console.log('getResponseFile: ', resp, resp.status, resp.id)
       if ( resp && resp.id ){
         resp.status == 'ENABLED'?
-        getProtectedFile(resp.id, resp.url, resp.name, callback)
-        : checkForStatus(resp.id, 5 * Number(1000), callback)
+        getProtectedFile(resp.id, resp.url, resp.name)
+        : checkForStatus(resp.id, 5 * Number(1000))
       } else throw new Error("can't go on without an id")
     }
   }
 
   // protectAFile
-  var protectAFile = function (blob, callback) {
+  var protectAFile = function (blob) {
     if ( blob && !anyPrivileges()){
       var formData = new FormData()
       , xhr = new XMLHttpRequest()
@@ -247,7 +296,7 @@ DT = (function (){
 
       xhr.open('POST', 'https://api.doctrackr.com/v1/documents', true);
       xhr.setRequestHeader('Authorization', ['Bearer', returnToken()].join(' '))
-      xhr.onload = getResponseFile(callback)
+      xhr.onload = getResponseFile
       xhr.send(formData)
     }else
       throw new Error('check the file or privileges given and try again')
@@ -258,6 +307,7 @@ DT = (function (){
     valid = false
     tokenData = {}
     clearTimeout(tokenTimer)
+    events.trigger = ['logout', 'success', { reason: reason }]
     if (reason) console.warn('reason: ', reason)
   }
 
@@ -270,7 +320,7 @@ DT = (function (){
   // module pattern
   return {
     initialize: init
-    , authorize: authorize
+    , login: authorize
     , getToken: returnToken
     , protectAFile: protectAFile
     , policy: {
